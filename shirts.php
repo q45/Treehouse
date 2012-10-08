d5177b982e3d4f87add2f1bbb73fc266100403ea
Quintin Smith <quintinsmith@gmail.com>
2012-10-08 17:16:11 -0600

    first commit

---
+++ shirts.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Mike's Full Catalog of T-shirts";
+$section = "shirts";
+include('inc/header.php');?>
+
+<div class="section page">
+    <h1>Q&rsquo;s Full Catalog of Shirts</h1>
+</div>
+
+
+<?php include('inc/footer.php');?>
